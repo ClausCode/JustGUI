1079487ef4b166dac2c18d6b9ef6fe27d3eb4f35
Claus <nclaus.code@gmail.com>
2022-07-12 16:15:56 +0300

    .gitignore update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
+    id("maven-publish")
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.serialization") version "1.6.21"
 }
@@ -16,10 +18,24 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ClausCode:JustGUI:1.0.0")
     compileOnly("com.github.Minestom:Minestom:$minestomVersion")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+}
+
+publishing {
+    publications {
+    }
+    repositories {
+        maven {
+            name = "JustGUI" //  optional target repository name
+            url = URI("https://jitpack.io")
+            credentials {
+                username = "ClausCode"
+                password = "my-password"
+            }
+        }
+    }
 }
 
 tasks.withType<KotlinCompile> {
